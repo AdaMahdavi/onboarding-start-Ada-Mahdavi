a3006a79849a13b97075f9535e27995cfc96dc59
AdaMahdavi <ayuyu.wrd@gmail.com>
2025-09-06 23:51:29 -0400

    fix project.v error

--- src/project.v
+++ src/project.v
@@ -6,8 +6,6 @@
 `default_nettype none
 
 module tt_um_uwasic_onboarding_Ada_Mahdavi(
-
-  assign uio_oe = 8'hFF;
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,6 +14,7 @@ module tt_um_uwasic_onboarding_Ada_Mahdavi(
   input  wire       ena,      // always 1 when the design is powered, so you can ignore it
   input  wire       clk,      // clock
   input  wire       rst_n     // reset_n - low to reset
+  );
 
   assign uio_oe = 8'hFF;
 
@@ -36,7 +35,7 @@ module tt_um_uwasic_onboarding_Ada_Mahdavi(
     .out({uio_out, uo_out})
   );
 
-);
+
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
